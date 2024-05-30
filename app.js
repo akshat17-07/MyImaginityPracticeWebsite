19a4b3d4ba32a017bfb0aadd927dcec73f07087b
Akshat Harlalka <akshatharlalka.ah@icloud.com>
2024-05-30 16:06:17 -0400

    Completed with bugs

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
-menu.addEventListener('click', function() {
+// Toggle mobile menu
+const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Close mobile menu when clicking on a menu item
+menuLinks.addEventListener('click', mobileMenu);
+
+// Close mobile menu when clicking outside of it
+document.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && !menuLinks.contains(event.target)) {
+        if (menu.classList.contains('is-active')) {
+            menu.classList.remove('is-active');
+            menuLinks.classList.remove('active');
+        }
+    }
 });
